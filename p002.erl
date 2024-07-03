d93087e86be51541459ee8153173ab5e704f55cd
INIT-REF <rufmichan@freenet.de>
2024-07-03 07:26:29 +0200

    Update

--- p002.erl
+++ p002.erl
@@ -4,7 +4,10 @@
 -export([solve/0]).
 
 fib(Limit) -> fib([2,1], Limit).
-fib([H|T], Limit) when H > Limit -> T;
-fib([H|T], Limit) -> fib([H + hd(T)] ++ [H|T], Limit).
+fib([H|T], Limit) -> 
+    case H > Limit of 
+        true -> T;
+        false -> fib([H + hd(T)] ++ [H|T], Limit)
+    end.
    
 solve() -> lists:sum([X || X <- fib(4000000), 0 == X rem 2]).
--- p003.erl
+++ p003.erl
@@ -6,7 +6,10 @@
 
 lpf(N) -> lpf(N, 3).
 lpf(1,D) -> D;
-lpf(N, D) when N rem D == 0 -> lpf(N div D, D);
-lpf(N, D) -> lpf(N, D + 2).
+lpf(N, D) -> 
+    case N rem D == 0 of
+        true -> lpf(N div D, D);
+        false -> lpf(N, D + 2)
+    end.
 
 solve() -> lpf(600851475143).
