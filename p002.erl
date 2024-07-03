0285b1e2a1fd6323e0cc935203c14ae4584110ad
INIT-REF <rufmichan@freenet.de>
2024-07-03 06:27:02 +0200

    Update

---
+++ p002.erl
@@ -0,0 +1,13 @@
+%% Solution to Project Euler problem 002
+
+-module(p002).
+-export([solve/0]).
+
+fib(Limit) -> fib([2,1], Limit).
+fib([H|T], Limit) -> 
+   case H > Limit of
+      true -> T;
+      false -> fib([H + hd(T)] ++ [H|T], Limit)
+   end.
+
+solve() -> lists:sum([X || X <- fib(4000000), 0 == X rem 2]).
