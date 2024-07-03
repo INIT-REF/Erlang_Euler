fd62de6608b1186eae436ff1f33970ec5a606e19
INIT-REF <rufmichan@freenet.de>
2024-07-03 08:02:54 +0200

    Update

---
+++ p005.erl
@@ -0,0 +1,11 @@
+%% Solution to Project Euler problem 005
+
+-module(p005).
+-export([solve/0]).
+
+gcd(0, B) -> B;
+gcd(A, B) -> gcd(B rem A, A).
+
+lcm(A, B) -> A * B div gcd(A, B).
+
+solve() -> lists:foldl(fun(A, B) -> lcm(A, B) end, 1, lists:seq(2, 20)).
