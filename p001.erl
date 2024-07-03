84321b51d3567d12d0074c2ef6655670fcb2e84b
INIT-REF <rufmichan@freenet.de>
2024-07-03 06:01:32 +0200

    Update

---
+++ p001.erl
@@ -0,0 +1,14 @@
+%% Solution to Project Euler problem 001
+
+-module(p001).
+-export([solve/0, alternative/0]).
+
+solve() -> lists:sum([X || X <- lists:seq(1, 999), 0 == X rem 3 * X rem 5]).
+
+
+%% Alternative using lists of multiples
+
+alternative() -> 
+    lists:sum(lists:seq(0, 999, 3) ++ 
+    lists:seq(0, 999, 5) -- 
+    lists:seq(0, 999, 15)).
