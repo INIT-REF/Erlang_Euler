35d3aaea6bd4ac14f9bb591c3613d195e06f6b61
INIT-REF <rufmichan@freenet.de>
2024-07-04 08:16:31 +0200

    Update

--- p002.erl
+++ p002.erl
@@ -4,10 +4,10 @@
 -export([solve/0]).
 
 fib(Limit) -> fib([2,1], Limit).
-fib([H|T], Limit) -> 
+fib([H,TH|T], Limit) -> 
     case H > Limit of 
-        true -> T;
-        false -> fib([H + hd(T)] ++ [H|T], Limit)
+        true -> [TH|T];
+        false -> fib([H + TH] ++ [H,TH|T], Limit)
     end.
    
 solve() -> lists:sum([X || X <- fib(4000000), 0 == X rem 2]).
