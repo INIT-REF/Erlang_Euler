c8126d95cc2b1325be226e2bb0d92ad6cf514b24
INIT-REF <rufmichan@freenet.de>
2024-07-04 05:08:50 +0200

    Update

--- p014.erl
+++ p014.erl
@@ -3,24 +3,20 @@
 -module(p014).
 -export([solve/0]).
 
-collatz(N) ->
+cseql(N) -> cseql(N, 1).
+cseql(1, L) -> L;
+cseql(N, L) ->
     case 0 == N rem 2 of
-        true -> [N div 2];
-        false -> [3 * N + 1]
+        true -> cseql(N div 2, L + 1);
+        false -> cseql(3 * N + 1, L + 1)
     end.
 
-cseql(C) ->
-   case hd(C) == 1 of
-       true -> length(C);
-       false -> cseql(collatz(hd(C)) ++ C)
-   end.
-
-longest_cseq(N) -> longest_cseq(N, cseql([N]), 0, 0).
+longest_cseq(N) -> longest_cseq(N, cseql(N), 0, 0).
 longest_cseq(1, _, _, Max) -> Max;
 longest_cseq(N, L, Maxl, Max) ->
     case L > Maxl of
-        true -> longest_cseq(N - 1, cseql([N - 1]), L, N);
-        false -> longest_cseq(N - 1, cseql([N - 1]), Maxl, Max)
+        true -> longest_cseq(N - 1, cseql(N - 1), L, N);
+        false -> longest_cseq(N - 1, cseql(N - 1), Maxl, Max)
     end.
 
 solve() -> longest_cseq(999999). 
