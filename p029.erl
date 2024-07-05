e3d639a039e884a2a8be5f92b3663cde764387ff
INIT-REF <rufmichan@freenet.de>
2024-07-05 09:37:48 +0200

    Update

--- p029.erl
+++ p029.erl
@@ -5,4 +5,4 @@
 
 l() -> lists:seq(2, 100).
 
-solve() -> length(lists:uniq([trunc(math:pow(A, B)) || A <- l(), B <- l()])).
+solve() -> length(lists:uniq([(math:pow(A, B)) || A <- l(), B <- l()])).
